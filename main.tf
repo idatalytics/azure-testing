c69f6c3f9a78b12fd356fa6d17ca64affea7a5f1
idatalytics <106198677+idatalytics@users.noreply.github.com>
2022-06-02 17:15:06 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,38 +1,15 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
 terraform {
-  required_version = ">= 0.13"
-  backend "azurerm" {
-    resource_group_name  = "REDACTED"
-    storage_account_name = "REDACTED"
-    container_name       = "tfbackend"
-    key                  = "terraform.state"
-  }
-  required_providers { #what does this mean and why is it required.
-    mongodbatlas = {
-      source = "mongodb/mongodbatlas"
-      version = "0.9.1"
-    }
-
+  required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "2.59.0"
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
     }
   }
 }
 
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-
-  subscription_id = var.subscription_id
-  client_id       = var.service_principal_app_Id
-  client_secret   = var.service_principal_password
-  tenant_id       = var.tenant_id
-}
-
-provider "mongodbatlas" {
-  public_key  = var.mongodb_atlas_api_pub_key
-  private_key = var.mongodb_atlas_api_pri_key
-}
-
-locals {
-  infra_version = "1.0"
 }
