efff88b567f91bb0e237bfd60f20ee064b782a5a
idatalytics <106198677+idatalytics@users.noreply.github.com>
2022-06-08 18:16:29 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -12,4 +12,8 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+  subscription_id = "ae5613de-f49e-47e9-afa0-54dae3dbb045"
+  client_id       = "b58913bd-12a8-4720-a540-74f0f6a600e8"
+  client_secret   = "b3d955d4-9342-4b8b-9116-d23b2b592737"
+  tenant_id       = "5b21e8fd-f562-4c52-9054-1a5c38b33db8"
 }
